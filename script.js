7d6469a97c33d8cda6ce9c400b27c42186ccdcfc
lazh22 <rapapap98@gmail.com>
2020-12-06 16:54:04 +0500

    G

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ $(document).ready(function () {
 });
 
 $("#btnSelect").click(function (e) {
-    let select = document.querySelector("select.custom-select")
+    let select = document.querySelector("select.custom-select");
     let url = select.options[select.selectedIndex].value + " " + "div#answer";
-    document.querySelector("#msgLabel").innerHTML = "Сообщение от пользователя " + select.options[select.selectedIndex].innerHTML;
+
+    let selectedUrl1 = select.options[select.selectedIndex].value;
+    let selectedUser = select.options[select.selectedIndex].innerHTML;
+    document.querySelector("#msgLabel").innerHTML = "Сообщение от пользователя " + "<a href=''" + selectedUrl1 +  "target='_blank'>" + selectedUser + "</a>";
 
     $(".modal-body").load(url, function (response, status, request) {
         document.querySelector(".modal-body>div").id="";
