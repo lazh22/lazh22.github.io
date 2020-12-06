c4ba243a726026832e21fcc542a60fe6e7ae61aa
lazh22 <rapapap98@gmail.com>
2020-12-06 14:11:20 +0500

    f

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
-    $("[data-toggle='collapse']").collapse();
+    $(".modal-body").load("https://artart241.github.io a.nav-link.active", function (response, status, request) {
+
+    });
+});
+$("#btnSelect").click(function (e) {
+    let select = document.querySelector("select.custom-select")
+    let url = select.optoins[select.selectedIndex].values + " " + "div#answer";
+
+    $(".modal-body").load(url, function (response, status, request) {
+        document.querySelector(".modal-body>div").id="";
+    });
 });
